58158fbd8b83181cad9936ce5f46fe08cfde408f
Admin <Admin@Admin-PC>
2018-04-08 11:39:49 +0600

    edited

--- src/RunEncap.java
+++ src/RunEncap.java
@@ -6,6 +6,6 @@ public class RunEncap {
       encap.setAge(20);
       encap.setIdNum("12343ms");
 
-      System.out.print("Name : " + encap.getName() + " Age : " + encap.getAge());
+      System.out.print("Name : " + encap.getName() + ", Age : " + encap.getAge() + ", Id : " + encap.getIdNum());
    }
 }
